067805d1e0b727ce6f705ef3c192267840526303
龙  张 <longzhang@longdeMacBook-Air.local>
2017-07-02 02:56:48 +0800

    完善

--- modules/allIds.js
+++ modules/allIds.js
@@ -3,12 +3,8 @@ import mongoose from 'mongoose'
 
 const idsSchema = new mongoose.Schema({
 	restaurant_id: Number,
-	food_id: Number,
-	order_id: Number,
 	user_id: Number,
 	address_id: Number,
-	cart_id: Number,
-	img_id: Number,
 	category_id: Number,
 	item_id: Number,
 	sku_id: Number, 
@@ -23,12 +19,8 @@ Ids.findOne((err, data) => {
 	if (!data) {
 		const newIds = new Ids({
 			restaurant_id: 0,
-			food_id: 0,
-			order_id: 0,
 			user_id: 0,
 			address_id: 0,
-			cart_id: 0,
-			img_id: 0,
 			category_id: 0,
 			item_id: 0,
 			sku_id: 0, 
